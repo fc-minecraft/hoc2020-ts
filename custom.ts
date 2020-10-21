25924a3156e98d26647300faa09f25ac93e5c9ec
neonerz <gerald@rewrite.media>
2020-10-21 01:32:17 -0400

    Update custom.ts

--- custom.ts
+++ custom.ts
@@ -9,12 +9,4 @@ namespace hoc2020 {
         agent.interact(FORWARD)
     }
 
-    /**
-     * Agent Interact Forward
-     */
-    //% block="flip lever 2"
-    export function buildFence() {
-        agent.interact(FORWARD)
-    }
-
 }
