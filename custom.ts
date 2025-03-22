58555cff4f02b30cc49dae36c9b6c5cc19e2480c
Envel <n.gucenkov@gmail.com>
2025-03-22 22:32:45 +0300

    1

--- custom.ts
+++ custom.ts
@@ -4,7 +4,7 @@ namespace hoc2020 {
     /**
      * Agent interact forward
      */
-    //% block="toggle lever"
+    //% block="переключить рычаг"
     export function flipLever() {
         agent.interact(FORWARD)
     }
@@ -12,17 +12,17 @@ namespace hoc2020 {
     /**
      * Agent place Iron Bars
      */
-    //% block="place iron bars down"
+    //% block="установить железные прутья вниз"
     export function placeFence() {
         agent.setItem(IRON_BARS, 1, 1)
         agent.place(DOWN)
-    }   
+    }
 
     /**
      * Agent till soil
      */
-    //% block="till and move forward %n times"
-    //% n.defl=1 
+    //% block="вспахать и двигаться вперед %n раз"
+    //% n.defl=1
     export function tillSoil(n: number): void{
         for (let i = 0; i < n; i++){
             agent.till(UP)
@@ -30,14 +30,14 @@ namespace hoc2020 {
                 "execute @c ~ ~ ~ detect ~ ~-1 ~ dirt 0 setblock ~ ~-1 ~ farmland 2"
             )
             agent.move(FORWARD,1)
-        }        
-    }         
-    
+        }
+    }
+
     /**
      * Agent place wood down
      */
-    //% block="move and place wood %n times"
-    //% n.defl=1 
+    //% block="двигаться и устанавливать дерево %n раз"
+    //% n.defl=1
     export function placePlanks(n: number): void{
 
         for (let i = 0; i < n; i++){
@@ -45,73 +45,73 @@ namespace hoc2020 {
 
             agent.setItem(PLANKS_OAK, 1, 1)
             agent.place(DOWN)
-        }        
-    } 
+        }
+    }
 
     /**
      * Agent place rail down
      */
-    //% block="place rail below"
+    //% block="установить рельс внизу"
     export function placeRails(){
         agent.setItem(RAIL, 1, 1)
         agent.place(DOWN)
-    }        
+    }
 
     /**
      * Lead ravager
      */
-    //% block="lead ravager %n forward"
-    //% n.defl=1 
+    //% block="вести опустошителя вперед %n раз"
+    //% n.defl=1
     export function leadRavager(n: number): void{
 
         for (let i = 0; i < n; i++){
             agent.move(FORWARD,1)
             //loops.pause(200)
-        }        
-    }    
+        }
+    }
 
     /**
      * Agent move
      */
-    //% block="agent move %d by %n"
-    //% n.defl=1 
+    //% block="движение агента %d на %n"
+    //% n.defl=1
     export function moveAgent(d: SixDirection, n: number): void{
         for (let i = 0; i < n; i++){
             agent.move(d,1)
-        }        
-    }     
+        }
+    }
 
     /**
      * Agent move up
      */
-    //% block="agent climb %n up"
-    //% n.defl=1 
+    //% block="подъем агента %n вверх"
+    //% n.defl=1
     export function agentClimb(n: number): void{
 
         for (let i = 0; i < n; i++){
             agent.move(UP,1)
-        }        
-    }                         
+        }
+    }
 
     /**
      * Give player sapling
      */
-    //% block="accept gift"
+    //% block="принять подарок"
     export function acceptGift(){
         mobs.give(
             mobs.target(LOCAL_PLAYER),
             OAK_SAPLING,
             1
         )
-    }  
+    }
 
     /**
      * Turn the Agent
      */
-    //% block="turn agent %t"
+    //% block="повернуть агента %t"
     export function turnAgent(t: TurnDirection): void{
         agent.turn(t);
-    }      
+    }
 }
 
 //%  block="HOC 2020 Cont" weight=200 color=#569138
@@ -120,7 +120,7 @@ namespace hoc2020Different {
     /**
      * Repeat code the specified number of times
      */
-    //% block="repeat $n times"
+    //% block="повторить $n раз"
     //% n.defl=2
     //% handlerStatement=1
     //% draggableParameters="reporter"
